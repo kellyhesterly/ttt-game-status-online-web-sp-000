a3b8a7800438317ebde4b74da53b041b1644bf5e
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-26 19:33:13 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,64 +1,39 @@
+require 'pry'
 # Helper Method
 def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
+  board[index] == "X" || board[index] == "O"
 end
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [
-[0,1,2],
-[3,4,5],
-[6,7,8],
-[0,4,8],
-[2,4,6],
-[0,3,6],
-[1,4,7],
-[2,5,8]
+WIN_COMBINATIONS =[
+  [2,5,8],
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7]
 ]
 
 def won?(board)
-WIN_COMBINATIONS.each do |single_index_won|
-  win_index_1 = single_index_won[0]
-  win_index_2 = single_index_won[1]
-  win_index_3 = single_index_won[2]
+  WIN_COMBINATIONS.detect do |combo|
 
-  character_1 = board[win_index_1]
-  character_2 = board[win_index_2]
-  character_3 = board[win_index_3]
+    position_1 = board[combo[0]]
+    position_2 = board[combo[1]]
+    position_3 = board[combo[2]]
 
-  if character_1 == character_2 && character_2 == character_3 && position_taken?(board, win_index_1)
-    return single_index_won
+  if position_1 == "X" && position_2 == "X" && position_3 == "X" ||
+     position_1 == "O" && position_2 == "O" && position_3 == "O"
+   return combo
+    end
   end
-end
-  return false
 end
 
 def full?(board)
-if board.all? {|character|
-  character == "X" || character == "O"}
-  return true
-end
-end
-
-def draw?(board)
-if full?(board) && !won?(board)
-  true
-elsif
-  !won?(board) && !full?(board)
-  false
-else
-  won?(board)
-  false
-end
-end
-
-def over?(board)
-  if won?(board) || draw?(board) || full?(board)
+  board.all? do |token|
+  if position_taken?(board, token)
     true
-end
-end
-
-def winner(board)
-  if won?(board)
-  return board [won?(board)[0]]
-end
-end
+      end
+    end
+  end
