a4b92c4f1c0dbe72cc3e0fdd7c5dcd1e14930c79
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-26 19:49:23 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -37,4 +37,11 @@ def full?(board)
 end
 
 def draw?(board)
+  if won?(board)
+    return false
+  else full?(board)
+    return true
+  elsif !won?(board) && !full?(board)
+    return false
+  end
 end
