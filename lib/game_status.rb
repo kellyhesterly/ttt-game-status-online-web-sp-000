80ea032cbe0a9b59fa31a078bb97aaddace68e6d
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-26 19:38:29 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,4 +16,16 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
+
+WIN_COMBINATIONS.detect do |combo|
+
+  position_1 = board[combo[0]]
+  position_2 = board[combo[1]]
+  position_3 = board[combo[2]]
+
+  if position_1 == "X" && position_2 == "X" && position_3 == "X" ||
+     position_1 == "O" && position_2 == "O" && position_3 == "O"
+     return combo
+  end
+ end
 end
